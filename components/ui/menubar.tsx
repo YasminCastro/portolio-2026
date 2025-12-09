77f1a9519e5238e21dc09f222539bbf7a148bf13
Yasmin <yasminsdcastro@gmail.com>
2025-12-09 12:34:35 -0300

    fix: warning

--- components/ui/menubar.tsx
+++ components/ui/menubar.tsx
@@ -17,6 +17,7 @@ function Menubar({
         "bg-background flex h-9 items-center gap-1 rounded-md border p-1 shadow-xs",
         className
       )}
+      suppressHydrationWarning
       {...props}
     />
   );
