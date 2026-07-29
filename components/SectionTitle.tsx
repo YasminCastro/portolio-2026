fe4a2331b1428f31f1dc1a7d8471cacbc4e0895f
Yasmin <yasminsdcastro@gmail.com>
2026-07-29 19:45:14 -0300

    fix: h2-h6 structure

--- components/SectionTitle.tsx
+++ components/SectionTitle.tsx
@@ -13,9 +13,9 @@ export default function SectionTitle({
     <div
       className={`flex flex-col items-center space-y-4 sm:space-y-6 md:space-y-8 ${className}`}
     >
-      <button className="px-3 py-1 sm:px-4 sm:py-1.5 rounded-full bg-linear-to-r from-[#A78BFA] via-[#60A5FA] to-[#FCA5A5] text-white font-medium text-xs sm:text-sm hover:opacity-90 transition-opacity shadow-lg shadow-[#A78BFA]/20">
+      <h2 className="px-3 py-1 sm:px-4 sm:py-1.5 rounded-full bg-linear-to-r from-[#A78BFA] via-[#60A5FA] to-[#FCA5A5] text-white font-medium text-xs sm:text-sm hover:opacity-90 transition-opacity shadow-lg shadow-[#A78BFA]/20">
         {title}
-      </button>
+      </h2>
       <p className="text-white/90 text-center text-sm sm:text-base md:text-lg max-w-2xl px-4">
         {description}
       </p>
